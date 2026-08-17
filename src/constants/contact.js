78f1aefcaae4f69d8c7139adaecf04ae79f3de49
Huda Khaled <hudakh1632002@gmail.com>
2026-08-17 09:13:17 +0300

    fix: update contact information

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,4 +1,4 @@
-export const WHATSAPP_PHONE = "96555969031";
+export const WHATSAPP_PHONE = "96596682816";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_PHONE}`;
 export const PHONE_TEL_URL = `tel:+${WHATSAPP_PHONE}`;
 
